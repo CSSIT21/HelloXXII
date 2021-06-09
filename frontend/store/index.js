3747a65ef4d80726d7aead6c7642fe9f4170dba3
Mr.SIRAWIT PRATOOMSUWAN <63130500227@st.sit.kmutt.ac.th>
2021-06-09 16:36:38 +0700

    Init frontend

---
+++ frontend/store/index.js
@@ -0,0 +1,22 @@
+import Vuex from "vuex";
+import {
+  getField,
+  updateField
+} from 'vuex-map-fields';
+
+const store = new Vuex.Store({
+  state: {
+
+  },
+  getters: {
+    getField
+  },
+  mutations: {
+    updateField
+  },
+  actions: {
+
+  }
+});
+
+export default store;

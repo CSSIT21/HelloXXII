62af5783e15a6fb04ed8e878b99cc86694a60ca8
Mr.SIRAWIT PRATOOMSUWAN <48322519+bronystylecrazy@users.noreply.github.com>
2021-06-10 23:15:00 +0700

    migrate from ts to js

--- backend/src/models/Insane.js
+++ backend/src/models/Insane.js
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-module.exports = thinky.createModel("Insane", {
+module.exports = thinky.createModel('Insane', {
     name: String,
     pair: String,
     code: String,
     hints: Array,
     co: String
 });
-=======
-const Insane = thinky.createModel('Insane', {
-	name: String,
-	pair: String,
-	code: String,
-	hints: Array,
-	co: String,
-});
-
-export default Insane;
->>>>>>> e3238667fe2b69046e098057411e5c7c6cbba520

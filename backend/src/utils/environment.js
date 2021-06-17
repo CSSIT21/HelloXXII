7d9a2f37e39011b2fa920e86dfe6d82991c72913
Mr.SIRAWIT PRATOOMSUWAN <48322519+bronystylecrazy@users.noreply.github.com>
2021-06-18 00:49:19 +0700

    ทำเชี่ยอะไรมึง

--- backend/src/utils/environment.js
+++ backend/src/utils/environment.js
@@ -1,5 +1,5 @@
 const environment = process.env.NODE_ENV || 'development';
-const dev = environment !== 'development';
+const dev = environment === 'development';
 const url = dev ? 'http://localhost:8081' : 'https://helloxxii-api.cscc.cf';
 module.exports = {
     dev,

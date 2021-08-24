7c93dd134023715a50ef74da5a1600087fbda015
Ms.MONTHARA KIATRUANGCHAI <monthara.k@mail.kmutt.ac.th>
2021-08-24 15:49:18 +0700

    Change logo to color

--- frontend/components/guess/Team.vue
+++ frontend/components/guess/Team.vue
@@ -1,30 +1,35 @@
 <template>
   <div class="team">
-    <img
-      :src="`https://storage.googleapis.com/helloxxii/icon/${role}.svg`"
-      alt="Your team logo"
-    />
-    <h6>{{ role }} team</h6>
+    <div class="color-circle" :style="`background-color: ${color_code}`"></div>
+    <h6>{{ color_name }} color</h6>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    role: "chicken",
+    color_name: "Lemon chiffon",
+    color_code: "#fffacd",
   }),
 };
 </script>
 
 <style scoped>
 .team {
-  text-align: center;
-  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
 }
 
-.team img {
+.team .color-circle {
   width: 80px;
   height: 80px;
+  margin: 15px 0;
+
+  border-radius: 50%;
+  border: 3.5px solid white;
+  box-shadow: 0px 1px 5px rgb(212, 212, 212);
 }
 
 .team h6 {
@@ -35,9 +40,10 @@ export default {
 }
 
 @media only screen and (max-width: 768px) {
-  .team img {
+  .team .color-circle {
     width: 65px;
     height: 65px;
+    border-width: 3px;
   }
 
   .team h6 {

0ed8ecb403a56ad7276111e10bf9c7233610463b
Mr.SIRAWIT PRATOOMSUWAN <48322519+bronystylecrazy@users.noreply.github.com>
2021-08-28 20:22:01 +0700

    Update kohi.js

--- frontend/middleware/kohi.js
+++ frontend/middleware/kohi.js
@@ -1,6 +1,6 @@
 export default function ({ store, redirect}){
   if((process.env.NODE_ENV || 'development') === 'development') return;
-  if (![3].includes(store.state.auth.profile.usertype)) {
+  if (![1].includes(store.state.auth.profile.usertype)) {
     return redirect('/mentor-overview');
   }
 }

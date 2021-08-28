3a76ceadd2fa4dac3e2bcc7f7b07a69a95b84b55
Mr.SIRAWIT PRATOOMSUWAN <48322519+bronystylecrazy@users.noreply.github.com>
2021-08-28 21:13:52 +0700

    Update route.js

--- frontend/middleware/route.js
+++ frontend/middleware/route.js
@@ -1,9 +1,9 @@
 export default function ({ store, redirect}){
   if((process.env.NODE_ENV || 'development') === 'development') return;
-  if (![1].includes(store.state.auth.profile.usertype)) {
+  if ([1].includes(store.state.auth.profile.usertype)) {
     return redirect('/pair-peer-mentor');
   }
-  if (![2,3].includes(store.state.auth.profile.usertype)) {
+  if ([2,3].includes(store.state.auth.profile.usertype)) {
     return redirect('/mentor-overview');
   }
   return redirect('/logout');

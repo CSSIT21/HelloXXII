fa3e72bed44d49f2dfc27f5bfd890a653a6977c3
Mr.SIRAWIT PRATOOMSUWAN <48322519+bronystylecrazy@users.noreply.github.com>
2021-08-28 20:27:17 +0700

    Update senpai.js

--- frontend/middleware/senpai.js
+++ frontend/middleware/senpai.js
@@ -1,6 +1,6 @@
 export default function ({ store, redirect}){
   if((process.env.NODE_ENV || 'development') === 'development') return;
-  if (![1,2].includes(store.state.auth.profile.usertype)) {
+  if (![2,3].includes(store.state.auth.profile.usertype)) {
     return redirect('/login');
   }
 }
